35acba4688eb713c8004d219797f165bffe059be
yuhao <yhwanan@foxmail.com>
2021-01-26 14:11:08 +0800

    修改接口地址

--- src/core/api.js
+++ src/core/api.js
@@ -1,35 +1,172 @@
 export default {
     apiUrlList: {
         user: {
-            get: {
-                url: "/api/salesman/currentUser",
-                option: { method: "get", host: "crm-health-api" }
-            },
-            buiness: {
-                url: "/api/app/user/businessLine/info",
+            get: "/api/sys/current_user",
+            goLogin: {
+                url: "/saml2/auth?sso",
                 option: { method: "get" }
             },
+            business: "/api/hrEmployee/getBusinessSeries",
+            // logout: "/saml2/auth/?slo",
             logout: {
                 url: "/api/sys/logout",
                 option: { method: "post" }
             },
-            role: {
-                url: "/api/medicsale-crm/1.0/role_type",
-                option: { method: "get", host: "apigw" }
-            },
-            personLevel: "/api/olap/currentPersonLevel",
-            datahub: "/datahub/report/get_report_data",
-            option: { method: "post", host: "crm-new" }
-        },
-        common: {
-            init: "/api/sys/window/common_init",
-            detail: {
-                url: "/api/sys/window/common_detail",
+            department: "/api/app/user/businessLine/info",
+            setDefaultDepartment: {
+                url: "/api/app/user/save/defBusinessLine",
                 option: { method: "post" }
             },
-            upload: "/api/sys/common/upload/",
+            role: {
+                url: "/api/app/user/getDingDingRole",
+                option: { method: "post" }
+            },
+            custLine: "/api/crmdrugstore/authorityBussinessList"
+        },
+        company: {
+            add: {
+                url: "/api/crmdrugstore/hzParties/OBDetail/add",
+                option: { method: "post" }
+            },
+            addBusiness: {
+                url: "/api/crmdrugstore/hzPartiesExts/BSDetail/add",
+                option: { method: "post" }
+            },
+            update: {
+                url: "/api/crmdrugstore/hzParties/OBDetail/update",
+                option: { method: "post" }
+            },
+            updateBusiness: {
+                url: "/api/crmdrugstore/hzPartiesExts/BSDetail/update",
+                option: { method: "post" }
+            },
+            detail: "/api/crmdrugstore/hzPartiesAndExtsAll",
+            detailBusiness: {
+                url: "/api/crmdrugstore/hzPartiesExts/BSDetail/info",
+                option: { method: "post" }
+            },
+            enableStatus: "/api/sys/lookup/codes/crm_drugstore_enable_status",
+            coopStatus: "/api/sys/lookup/codes/crm_drugstore_coop_status",
+            seaType: "/api/sys/lookup/codes/crm_drugstore_sea_type",
+            orgType: "/api/sys/lookup/codes/crm_drugstore_org_typed",
+            type: "/api/sys/lookup/codes/crm_drugstore_cust_type", // 客户类型
+            release: {
+                url: "/api/crmdrugstore/batchupdate_form/3",
+                option: { method: "post" }
+            },
+            pickUp: {
+                url: "/api/crmdrugstore/batchupdate_form/1",
+                option: { method: "post" }
+            }
+        },
+        drugstore: {
+            add: {
+                url: "/api/crmdrugstore/hzPartySites/main/save",
+                option: { method: "post" }
+            },
+            employeecenter: "/api/hrEmployee/subFlag",
+            addBusiness: {
+                url: "/api/crmdrugstore/hzPartySites/business/save",
+                option: { method: "post" }
+            },
+            detail: "/api/crmdrugstore/hzPartySites/main/query",
+            detailBusiness: "/api/crmdrugstore/hzPartySites/business/query",
+            release: {
+                url: "/api/crmdrugstore/batchupdate_form/4",
+                option: { method: "post" }
+            },
+            pickUp: {
+                url: "/api/crmdrugstore/batchupdate_form/2",
+                option: { method: "post" }
+            },
+            kpCode: "/api/sys/lookup/codes/crm_drugstore_kp_type",
+            type: "/api/sys/lookup/codes/crm_drugstore_store_type",
+            channelList: "/api/sys/lookup/codes/crm_drugstore_channels?prescripte_channels"
+        },
+        contract: {
+            detail: "/api/sys/window/common_detail/113",
+            detailSale: "/api/sys/window/common_detail/255",
+            detailCoasaas: "/api/sys/window/common_detail/266",
+            serviceTypeSale: "/api/sys/lookup/codes/open_status?service1_flag",
+            packageType: "/api/sys/lookup/codes/crm_drugstore_package_types",
+            serviceStatus: "/api/sys/lookup/codes/crm_drugstore_service_status",
+            serviceType: "/api/sys/lookup/codes/crm_drugstore_server_type",
+            // 特批类型
+            specialType: "/api/sys/lookup/codes/crm_drugstore_special_type",
+            specialTypeCoasaas: "/api/sys/lookup/codes/crm_drugstore_special_ctype",
+            // 分佣比例
+            consortium: "/api/sys/lookup/codes/crm_drugstore_consortium",
+            // 结算方式
+            clearWay: "/api/sys/lookup/codes/crm_drugstore_clear_way",
+            // 智云合作方式 商盟saas
+            coopWay: "/api/sys/lookup/codes/crm_durgstore_coop_way",
+            companyServerType: "/api/sys/lookup/codes/crm_drugstore_server_typed",
+            type: "/api/sys/lookup/codes/crm_drugstore_contract_type?contract_type",
+            payWay: "/api/sys/lookup/codes/crm_drugstore_pay_way?pay_way",
+            connectWay: "/api/sys/lookup/codes/crm_drugstore_consulte_way",
+            tradeInfo: "/api/crmdrugstore//payment/info",
+            sign: "/api/electronicSignature/enterprise/fistStepSign",
+            downFile: "/api/electronicSignature/loadContract",
+            openFile: "/api/electronicSignature/lookUpContract",
+            getCode: {
+                url: "/api/sys/sequence/get",
+                option: { method: "post" }
+            },
+            addPay: {
+                url: "/api/crmdrugstore/create/trade",
+                option: { method: "post" }
+            },
+            confirmPay: {
+                url: "/api/crmdrugstore/payment/confirm",
+                option: { method: "post" }
+            },
+            updatePay: {
+                url: "/api/crmdrugstore/update/trade",
+                option: { method: "post" }
+            },
+            cancelPay: "/api/crmdrugstore/payment/cancel"
+        },
+        agreement: {
+            detail: "/api/sys/window/common_detail/209"
+        },
+        visit: {
+            contact: "/api/sys/lookup/basetypesubinfo/hz_party_contact_info",
+            way: "/api/sys/lookup/codes/crm_drugstore_visit_way",
+            wayd: "/api/sys/lookup/codes/crm_drugstore_visit_wayd",
+            detail: "/api/sys/window/common_detail/112",
+            content: "/api/sys/lookup/codes/crm_drugstore_visit_content",
+            plan: "/api/sys/lookup/codes/crm_drugstore_follow_plan?follow_pland",
+            will: "/api/sys/lookup/codes/crm_drugstore_client_will?client_will",
+            erp: "/sys/lookup/codes/crm_drugstore_erp_platform"
+        },
+        home: {
+            count: {
+                url: "/api/olap/getAggregateData",
+                option: { method: "post" }
+            },
+            warn: "/api/app/index/warn",
+            week: {
+                url: "/api/olap/getCurrentWeek",
+                option: { method: "get" }
+            },
+            workOrder: {
+                url: "/api/sys/bpm/crm/indexForNum",
+                option: { method: "get" }
+            },
+            all: {
+                url: "/api/app/index",
+                option: { method: "get" }
+            }
+        },
+        statistics: {
+            personLevel: {
+                url: "/api/sys/currentPersonLevel",
+                option: { method: "post" }
+            }
+        },
+        common: {
             datahub: {
-                url: "/datahub/report/get_report_data",
+                url: "/report/get_report_data",
                 option: { method: "post" }
             },
             queryAmount: {
@@ -37,12 +174,15 @@ export default {
                 option: { method: "post" }
             },
             regionMatch: {
-                url: "/datahub/report/open/crm_region_match",
+                url: "/report/open/crm_region_match",
                 option: { method: "post" }
             },
             menu: "/api/sys/menu",
+            sys: "/api/sys/window/common_init",
+            add: "/api/sys/window/common_detail",
             code: "/api/sys/lookup/codes/yesno",
             config: "/api/sys/window/window_config",
+            upload: "/api/sys/common/upload/",
             submit: "/api/sys/bpm/submit",
             token: "/api/sys/get_token",
             region: "/api/sys/lookup/baseinfo_sublist/drugstore_region",
--- src/core/index.js
+++ src/core/index.js
@@ -1,63 +1,21 @@
-import apiJSON from "./api.js";
-import is from "is";
+import apiUrlList from "./api.js";
 import copy from "./copy";
-import extend from "./extend";
 import axios from "axios";
-import cookie from "./cookie";
 import dateFilter from "./dateFilter";
 
-let goLogin = () => {
-    let host = window.location.host;
+let getBaseUrl = (origin = "gateway") => {
     let protocol = location.protocol;
-    let proOrigin = "moblie-drug-sales/";
-    let loginEnv = "";
-    if (/qa.91jkys/.test(host)) {
-        loginEnv = ".qa";
-    }
-    if (/pre.91jkys/.test(host)) {
-        loginEnv = ".pre";
-    }
-    window.location.href = `${protocol}//employee${loginEnv}.91jkys.com/sso/login?redirect=${protocol}//${host}/${proOrigin}`;
-};
-
-let getBaseUrl = (hostname = "crm-new") => {
+    let host = window.location.host;
     let apiEvn = "";
-    let protocol = location.protocol;
-    let host = window.location.host;
-    if (/qa.91jkys/.test(host)) {
-        apiEvn = ".qa";
+    if (/qa.zu-ji/.test(host)) {
         protocol = "https:";
-    } else if (/static.pre.91jkys/.test(host)) {
-        apiEvn = ".pre";
+        apiEvn = "-qa";
     }
-    let baseUrl = `${protocol}//${hostname}${apiEvn}.91jkys.com`;
+    if (/pre.zu-ji/.test(host)) {
+        apiEvn = "-pre";
+    }
+    let baseUrl = `${protocol}//${origin}${apiEvn}.zu-ji.com`;
     return baseUrl;
-};
-
-let loading = {
-    continue: 0,
-    show() {
-        if (!this.continue) {
-            // this.toast = Toast.loading({
-            //     message: "加载中",
-            //     duration: 0
-            // });
-            this.continue++;
-        } else {
-            this.continue++;
-        }
-        this.timeOut = setTimeout(() => {
-            this.hide();
-        }, 60000);
-    },
-    hide() {
-        clearTimeout(this.timeOut);
-        this.continue--;
-        if (this.continue <= 0) {
-            this.continue = 0;
-            this.toast && this.toast.clear();
-        }
-    }
 };
 
 let apiRequest = (url, { data, option }) => {
@@ -71,13 +29,13 @@ let apiRequest = (url, { data, option }) => {
     if (url.indexOf("http") == 0) {
         url = url.replace("http:", location.protocol);
     } else {
-        url = `${getBaseUrl(option.host)}${url}`;
+        url = `${getBaseUrl(option.origin)}${url}`;
     }
-    !option.noLoading && loading.show();
     let requestData = {
         method: option.method.toLocaleLowerCase(),
         url: url,
-        withCredentials: true
+        withCredentials: true,
+        headers: {}
     };
     if (option.headers) {
         requestData.headers = Object.assign(requestData.headers, option.headers);
@@ -94,50 +52,31 @@ let apiRequest = (url, { data, option }) => {
     } else {
         requestData.data = data;
     }
-    if (option.headers) {
-        requestData.headers = option.headers;
-    }
     return new Promise((resolve, reject) => {
         axios(requestData)
             .then(res => {
                 let data = res.data || {};
-                let error = data.error || {};
-                if (
-                    typeof data == "string" ||
-                    data.returnCode == "0000" ||
-                    data.returnCode == 0 ||
-                    data.code == 200 ||
-                    data.ok
-                ) {
-                    resolve(data.ok ? data.result : data);
-                } else if (data.returnCode == "0100" || error.code == 401) {
-                    goLogin();
-                    reject(data);
-                } else if (error.code == "402") {
-                    // error.message && Notify(error.message);
-                    reject(error);
-                } else {
-                    // data.returnMsg && Notify(data.returnMsg);
-                    reject(data);
+                if (typeof data == "string" || data.returnCode == "0000" || data.returnCode == 0 || data.ok) {
+                    resolve(data);
+                    return;
                 }
-                loading.hide();
             })
             .catch(res => {
-                loading.hide();
-                reject(res.response.data || {});
+                reject(res.data || {});
             });
     });
 };
 
 // 请求封装
 let api = {
-    static: apiJSON.static
+    static: apiUrlList.static
 };
-Object.keys(apiJSON.apiUrlList).forEach(itemFirst => {
+
+Object.keys(apiUrlList.apiUrlList).forEach(itemFirst => {
     api[itemFirst] = {};
-    Object.keys(apiJSON.apiUrlList[itemFirst]).forEach(item => {
-        let configApi = apiJSON.apiUrlList[itemFirst][item];
-        api[itemFirst][item] = (function (configApi) {
+    Object.keys(apiUrlList.apiUrlList[itemFirst]).forEach(item => {
+        let configApi = apiUrlList.apiUrlList[itemFirst][item];
+        api[itemFirst][item] = (function(configApi) {
             /*
                 把api中的每一个值转换为方法，例如下
                  apiUrlList: {
@@ -145,28 +84,17 @@ Object.keys(apiJSON.apiUrlList).forEach(itemFirst => {
                         get: "/api/sys/current_user"
                      }
                  }
-                 api[itemFirst][item] 相当于上面对像中 apiUrlList.user.get 转换为下面返回的函数，并接受两个参数data、option
+                 api[itemFirst][item] 即 apiUrlList.user.get 转换为下面返回的函数，并接受两个参数data、option
             */
-            let option = apiJSON.apiUrlList[itemFirst].option;
-            if (option && item != "opiont") {
-                if (typeof configApi == "string") {
-                    configApi = {
-                        url: configApi,
-                        option: option
-                    };
-                } else {
-                    configApi.option = Object.assign({}, option, configApi.option || {});
-                }
-            }
-            return function (data = {}, option = {}) {
+            return function(data = {}, option = {}) {
                 let action = configApi;
                 option = Object.assign({ method: "get", fullUrl: "" }, option);
-                if (typeof configApi == "object") {
+                if (typeof action == "object") {
                     action = configApi.url;
-                    option = Object.assign({}, option, configApi.option || {});
+                    Object.assign(option, configApi.option || {});
                 }
                 if (data.type == "option") {
-                    option = Object.assign({}, option, data);
+                    Object.assign(option, data);
                     data = {};
                 }
                 // action为接口地址 url 如上面的 "/api/sys/current_user" (apiUrlList.use.get)
@@ -179,36 +107,7 @@ Object.keys(apiJSON.apiUrlList).forEach(itemFirst => {
     });
 });
 
-function log() {
-    for (let key in arguments) {
-        arguments[key] ? console.log(JSON.parse(JSON.stringify(arguments[key]))) : console.log(arguments[key]);
-    }
-}
-
-function routeGoBack(option) {
-    let from = option.routeFrom || {};
-    if (!from.name || from.path == "/") {
-        // 详情页面直接刷新 然后点击后退
-        option.PARENT_ENTER &&
-            option.$router.replace({
-                path: option.PARENT_ENTER,
-                query: from.query
-            });
-    } else {
-        option.$router.back();
-    }
-}
-
 export default {
-    axios: axios,
-    goLogin: goLogin,
     api: api,
-    is: is,
-    console: log,
-    copy: copy,
-    extend: extend,
-    getBaseUrl: getBaseUrl,
-    routeGoBack: routeGoBack,
-    dateFilter: dateFilter,
-    cookie
+    dateFilter: dateFilter
 };
